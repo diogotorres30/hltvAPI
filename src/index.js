b27a6e28cc41a72d534bc58dcc208112ba0067f6
diogotorres30 <diogo.torres.30@gmail.com>
2019-09-27 17:22:54 +0100

    examples

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 const { HLTV } = require('hltv')
 
-HLTV.getMatch({id: 2306295}).then(res => {
-    console.log(res.players);
+HLTV.getTeamRanking({ year: '2017', month: 'may', day: '29' }).then((res) => {
+    console.log(res);
 })
 
-console.log("HelloWorld!");
+// HLTV.getMatch({ id: 2306295 }).then(res => {
+//     console.log(res.players);
+// })
+
+
+// HLTV.getEvent({ id: 3389 }).then(res => {
+//     console.log(res);
+// })
+
+// HLTV.getStreams().then((res) => {
+//     console.log(res);
+// })
