77501f0f53f1603cc7c94a189c8479ea6922668a
Diogo Torres <diogo.torres.30@gmail.com>
2019-10-03 14:32:48 +0100

    jsonify

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-const { HLTV } = require('hltv')
+const { HLTV } = require('hltv');
+
+const fs = require('fs');
+
+function jsonify(ctnt) {
+	var jsonContent = JSON.stringify(ctnt);
+	console.log(jsonContent);
+
+	fs.writeFile('output.json', jsonContent, 'utf8', function(err) {
+		if (err) {
+			console.log('An error occured while writing JSON Object to File.');
+			return console.log(err);
+		}
+	});
+}
+
+// HLTV.getMatch({ id: 2306295 }).then((res) => {
+// 	jsonify(res.players);
+// });
 
 HLTV.getTeamRanking({ year: '2017', month: 'may', day: '29' }).then((res) => {
-    console.log(res);
+    jsonify(res);
 })
-
-// HLTV.getMatch({ id: 2306295 }).then(res => {
-//     console.log(res.players);
-// })
-
 
 // HLTV.getEvent({ id: 3389 }).then(res => {
 //     console.log(res);
